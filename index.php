4890c9434efe4b91bed81e14fd71c521c2483aa9
Viridiana Jazel Amezcua Orona <viridiana.ao.04@gmail.com>
2016-06-29 21:17:36 -0500

    add static page for home

--- index.php
+++ index.php
@@ -1,17 +1,234 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>OpenStack Hackaton</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link href="openstack/css/menu_sideslide.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="openstack/css/style.css" />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Fira+Mono:400,700' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div class="menu-wrap">
+      <nav class="menu">
+        <div class="cl-effect-1 icon-list">
+          <a href="#" class="txt-center">Inicio</a>
+          <a href="#" class="txt-center">Tem&aacute;ticas</a>
+          <a href="#" class="txt-center">Acerca De</a>
+          <a href="#" class="txt-center">Sponsors</a>
+          <a href="#" class="txt-center">FAQ</a>
+          <a href="#" class="txt-center">Contacto</a>
+          <ul class="social-icons txt-center">
+            <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+            <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+            <li><a href="#"><span class="fa fa-slack"></span></a></li>
+          </ul>
+        </div>
+      </nav>
+      <button class="close-button pointer" id="close-button">Cerrar Menu</button>
+    </div>
+    <div class="header mobile">
+      <div class="logo mobile"></div>
+      <button class="menu-button pointer" id="open-button">Abrir Menu</button>
+    </div>
+    <main class="column">
+      <div class="content-wrap">
+        
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+        <div>
+          <!-- intro -->
+          <div id="hackathon-intro">
+            <div id="intro-date">
+              09-11 <br />
+              sept<br />
+              2016 <br />
+            </div>
+            <div class="mexico-map"><img src="openstack/img/mexico_map_gray.png" /></div>
+            <div id="intro-txt">
+              
+            </div>
+          </div>
+          <!-- /intro -->
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+          <!-- topics -->
+          <div id="hackathon-topics" class="container">
+            <h2 class="title txt-center">40 hrs para construir la mejor cloud app</h2>
+            <h3 class="subtitle">6 tem&aacute;ticas para que desarrolles tu aplicaci&oacute;n</h3>
+
+            <div class="grid">
+              <div class="grid-item border-top border-left border">
+                <h4 class="grid-title">IoT</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item border-top border-left border">
+                <h4 class="grid-title">MEDIA AND VIDEO</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item border-top border-left border-right border">
+                <h4 class="grid-title">HEALTH</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item border-top border-left border-bottom border">
+                <h4 class="grid-title">SOCIAL INNOVATION</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item border-top border-left border-bottom border">
+                <h4 class="grid-title">FINTECH</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item border-top border-left border-bottom border-right border">
+                <h4 class="grid-title">MAKERS</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+            </div>
+          </div>
+          <!-- /topics -->
+
+          <!-- about -->
+          <div id="hackathon-about" class="section with-sidebar clearfix container">
+            <div class="custom-sidebar column">
+              <h2 class="title txt-left">Acerca del OpenStack Hackathon</h2>
+            </div>
+
+            <div class="content column">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Donec elementum ligula eu sapien consequat eleifend. Donec nec dolor erat, condimentum sagittis sem. Praesent porttitor porttitor risus, dapibus rutrum ipsum gravida et. Integer lectus nisi, facilisis sit amet eleifend nec, pharetra ut augue. Integer quam nunc, consequat nec <a href="#">egestas ac</a>, volutpat ac nisi. Sed consectetur dignissim dignissim. Donec pretium est sit amet ipsum fringilla feugiat.</p>
+            </div>
+          </div>
+          <!-- /about -->
+
+          <!-- faq -->
+          <div id="hackathon-faq" class="section container">
+            <h2 class="title txt-left">FAQ</h2>
+
+            <div>
+              <div class="grid">
+              <div class="grid-item">
+                <h4 class="grid-title">IoT</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item">
+                <h4 class="grid-title">MEDIA AND VIDEO</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item">
+                <h4 class="grid-title">HEALTH</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item">
+                <h4 class="grid-title">SOCIAL INNOVATION</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item">
+                <h4 class="grid-title">FINTECH</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+
+              <div class="grid-item">
+                <h4 class="grid-title">MAKERS</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+              </div>
+            </div>
+            </div>
+          </div>
+          <!-- /faq -->
+
+          <!-- about -->
+          <div id="hackathon-hack" class="section with-sidebar clearfix container">
+            <div class="custom-sidebar column">
+              <h2 class="title txt-left">Hack en un bit</h2>
+            </div>
+
+            <div class="content column">
+              <div class="border-top border-left border-right clearfix border item">
+                <div class="custom-sidebar column label">
+                  Cuando
+                </div>
+                <div class="content column desc">
+                  Septiembre 09-11
+                </div>
+              </div>
+              <div class="border-top border-left border-right clearfix border item">
+                <div class="custom-sidebar column label">
+                  Donde
+                </div>
+                <div class="content column desc">
+                  Tecnologico de Monterrey Campus GDL
+                </div>
+              </div>
+
+              <div class="border-top border-left border-right clearfix border item">
+                <div class="custom-sidebar column label">
+                  El reto
+                </div>
+                <div class="content column desc">
+                  Desarrollar la mejor cloud app
+                </div>
+              </div>
+
+              <div class="border-top border-left border-right clearfix border item">
+                <div class="custom-sidebar column label">
+                  Entrada
+                </div>
+                <div class="content column desc">
+                  Gratis
+                </div>
+              </div>
+
+              <div class="border-top border-left border-right border-bottom clearfix border item">
+                <div class="custom-sidebar column label">
+                  Participantes
+                </div>
+                <div class="content column desc">
+                  Equipos de 3 a 6 integrantes
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /about -->
+        </div>
+        <!-- /container -->
+      </div>
+    </main>
+
+    <!-- footer -->
+    <div class="desktop footer txt-center">
+      <a href="#" class="btn">Participantes</a>
+      <a href="#" class="btn">Quiero ser patrocinador</a>
+      <a href="#" class="btn">Voluntarios</a>
+    </div>
+    <!-- /footer -->
+
+    <aside class="desktop">
+      <div class="logo"></div>
+      <nav class="menu-desktop">
+        <div class="cl-effect-1">
+          <a href="#">Inicio</a>
+          <a href="#">Tem&aacute;ticas</a>
+          <a href="#">Acerca De</a>
+          <a href="#">Sponsors</a>
+          <a href="#">FAQ</a>
+          <a href="#">Contacto</a>
+        </div>
+      </nav>
+      <ul class="social-icons">
+        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+        <li><a href="#"><span class="fa fa-slack"></span></a></li>
+      </ul>
+    </aside>
+
+    <script src="openstack/js/classie.js"></script>
+    <script src="openstack/js/main.js"></script>
+  </body>
+</html>
