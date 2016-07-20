a67f7cb45d08a6864ac3060cc6f6737cfd29592a
Viridiana J Amezcua Orona <viridiana.j.amezcua.orona@intel.com>
2016-07-20 10:24:32 -0500

    adding button to register in the pre-trainings and description of the themes

--- wp-content/themes/one-page/templates/homepage-services.php
+++ wp-content/themes/one-page/templates/homepage-services.php
@@ -2,11 +2,20 @@
 <section id="registrate" class="section_3">
    <div class="container">
 
-        <div class="register">
-            <a href="https://www.eventbrite.com/e/cloud-app-hackathon-guadalajara-mexico-registration-25823714425" target="_blank" class="btn">
-              Registrate
-            </a>
-        </div>
+        <div class="register row">
+        	<div class="col-md-6 col-xs-12">
+	            <a href="https://www.eventbrite.com/e/cloud-app-hackathon-guadalajara-mexico-registration-25823714425" target="_blank" class="btn">
+	           	Regístrate en el hackathon
+	            </a>
+	        </div>
+
+         	<div class="col-md-6 col-xs-12">
+	            <a href="http://www.eventbrite.com/o/openstack-hackathons-11054955763" target="_blank" class="btn">
+	              Regístrate en los entrenamientos previos
+	            </a>
+	        </div>
+	    </div>
+
     </div>
 </section>
 <!--/Register-->
--- wp-content/themes/one-page/templates/homepage-videos.php
+++ wp-content/themes/one-page/templates/homepage-videos.php
@@ -7,32 +7,35 @@
         <div class="grid">
           <div class="grid-item border-top border-left border">
             <h4 class="grid-title">IoT/WEARABLE/DRONES</h4>
-            <p></p>
+            <p>En esta categor&iacute;a caen todas las aplicaciones que hacen uso de sistemas embebidos, de drones o de sistemas port&aacute;tiles para la recolección de información para la nube. Por lo general son aplicaciones que hacen uso de las capacidades de cómputo de la nube para procesar la información recolectada.</p>
           </div>
 
           <div class="grid-item border-top border-left border">
             <h4 class="grid-title">MEDIA AND VIDEO</h4>
-            <p></p>
+            <p>En esta categor&iacute;a caen todas las aplicaciones que administren, almacenen y procesen informaci&oacute;n multimedia. Normalmente, aplicaciones en esta categoría harán uso de componentes como glance y swift.</p>
           </div>
 
           <div class="grid-item border-top border-left border-right border">
             <h4 class="grid-title">SALUD</h4>
-            <p></p>
+            <p>En esta categor&iacute;a caen todas las aplicaciones enfocadas a resolver problemas de salud. Estos pueden ser tanto administrativos como técnicos. Además se incluyen todas las aplicaciones que hagan análisis de datos de este tipo por medio de una nube.
+</p>
           </div>
 
           <div class="grid-item border-top border-left border-bottom border">
             <h4 class="grid-title">INNOVACI&Oacute;N SOCIAL</h4>
-            <p></p>
+            <p>En esta categor&iacute;a caen todas las aplicaciones enfocadas a resolver problemas sociales. Estos pueden ser en el &aacute;mbito gubernamental, de seguridad p&uacute;blica y de programas sociales. Además se incluyen todas las aplicaciones que hagan an&aacute;lisis de datos de este tipo por medio de una nube.
+</p>
           </div>
 
           <div class="grid-item border-top border-left border-bottom border">
-            <h4 class="grid-title">FINTECH</h4>
-            <p></p>
+            <h4 class="grid-title">TECNOLOG&Iacute;A FINANCIERA</h4>
+            <p>En esta categor&iacute;a caen todas las aplicaciones enfocadas a resolver problemas financieros. Estos pueden ser en el &aacute;mbito bancario, accionario y empresarial. Adem&aacute;s se incluyen todas las aplicaciones que hagan an&aacute;lisis de datos de este tipo por medio de una nube.
+</p>
           </div>
 
           <div class="grid-item border-top border-left border-bottom border-right border">
             <h4 class="grid-title">BIGDATA/ANAL&Iacute;TICOS</h4>
-            <p></p>
+            <p>En esta categoría caen todas las aplicaciones enfocadas a resolver problemas con una cantidad enorme de información. Estos pueden ser en cualquier ámbito, siempre y cuando la aplicación maneje cantidades de datos masivas. Además se incluyen todas las aplicaciones que hagan análisis de estos datos por medio de una nube.</p>
           </div>
         </div>
       </div>
