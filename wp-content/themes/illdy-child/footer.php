c495c3eeb477bd3fe011bb96f81198ea0ed3bde6
Noelia Nunez <nbnunezl@nzitron-mobl1.ger.corp.intel.com>
2016-08-27 12:47:31 -0500

    Adding GA

--- wp-content/themes/illdy-child/footer.php
+++ wp-content/themes/illdy-child/footer.php
@@ -93,6 +93,18 @@ if ( current_user_can( 'edit_theme_options' ) ) {
 				</div>
 			</div>
 		</footer><!--/#footer-->
+
+		<!--Google analitycs-->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-80190808-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 		<?php wp_footer(); ?>
 	</body>
 </html>
