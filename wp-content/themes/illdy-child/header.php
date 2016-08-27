d9eaff3cae2f45f0e0f9982aa340f4a60abe9f94
Noelia Nunez <nbnunezl@nmvaldez-mobl1.amr.corp.intel.com>
2016-08-27 17:15:09 -0500

    Fixed logo and mobile menu

--- wp-content/themes/illdy-child/header.php
+++ wp-content/themes/illdy-child/header.php
@@ -29,7 +29,7 @@ $preloader_enable = get_theme_mod( 'illdy_preloader_enable', 1 );
 			<div class="top-header">
 				<div class="container">
 					<div class="row">
-						<div class="col-xs-2">
+						<div class="col-xs-4">
 							<?php if( $logo ): ?>
 								<?php echo $logo ?>
 							<?php else: ?>
